51ef0e28a51274d0e5c62ff40944496662861dd8
jycamus90 <jycamus90@gmail.com>
2016-06-16 11:23:16 -0700

    Add a switch statement in attraction method
    Add a switch statement in attraction method

--- src/main/java/org/spideruci/cerebro/layout/NewSpringBoxNodeParticle.java
+++ src/main/java/org/spideruci/cerebro/layout/NewSpringBoxNodeParticle.java
@@ -13,14 +13,19 @@ import org.miv.pherd.geom.Point3;
 
 public class NewSpringBoxNodeParticle extends SpringBoxNodeParticle {
 
+	public static enum F_CONFIG{
+		A, B, C, D, E
+	}
+	
+	F_CONFIG fValue = F_CONFIG.A;
+		
+	
 	public NewSpringBoxNodeParticle(SpringBox box, String id) {
 		super(box, id);
-		// TODO Auto-generated constructor stub
 	}
 
 	public NewSpringBoxNodeParticle(SpringBox box, String id, double x, double y, double z) {
 		super(box, id, x, y, z);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
@@ -38,17 +43,43 @@ public class NewSpringBoxNodeParticle extends SpringBoxNodeParticle {
 				delta.set(opos.x - pos.x, opos.y - pos.y, is3D ? opos.z - pos.z
 						: 0);
 
-				double len = delta.normalize();
-				double k = box.getK() * edge.weight;
-				double factor = box.getK1() * (len - k); // original source
+				double len = delta.normalize(); // original code
+				double k = box.getK() * edge.weight; // original code
+				double ideal_len = 1; // box.getK() 
+				double factor;
+				
+				switch(fValue){
+					case A:
+						factor = box.getK1() * (len - k); // original code
+						break;
+					case B:
+						factor = box.getK1() * (len - ideal_len) * edge.weight;
+						break;
+					case C:
+						factor = box.getK1() * Math.log(len / (ideal_len * edge.weight));
+						break;
+					case D:
+						factor = box.getK1() * Math.log((ideal_len * edge.weight) / len);
+						break;
+					case E:
+						factor = box.getK1() * edge.weight;
+						break;
+					default:
+						factor = 0;
+
+				}
+				
+				
+				
+//				double factor = box.getK1() * (len - k); // original code
 				
 //				double factor = box.getK1() * edge.weight * edge.weight;
 //				double factor = box.getK1() * Math.log10(edge.weight);
 //				double factor = box.getK1() * Math.sqrt(edge.weight);
 
 //				 delta.scalarMult( factor );
-//				delta.scalarMult(factor * (1f / (neighbourCount * 0.1f)));
-				delta.scalarMult(factor * (1f / (neighbourCount)));
+				delta.scalarMult(factor * (1f / (neighbourCount * 0.1f)));
+//				delta.scalarMult(factor * (1f / (neighbourCount)));
 				// ^^^ XXX NEW inertia based on the node degree. This is one
 				// of the amelioration of the Spring-Box algorithm. Compare
 				// it to the Force-Atlas algorithm that does this on
@@ -85,7 +116,7 @@ public class NewSpringBoxNodeParticle extends SpringBoxNodeParticle {
 						len = box.getK(); // XXX NEW To prevent infinite
 									// repulsion.
 				
-					double factor = ((box.getK2() / (len * len)) * node.weight); //original source
+					double factor = ((box.getK2() / (len * len)) * node.weight); //original code
 //					double factor = box.getK2() * Math.log10(node.weight);
 //					double factor = box.getK2() * Math.sqrt(node.weight);
 //					double factor = box.getK2() * node.weight;
